354d16c13dad5bc505dce4a98af1806477651bff
DANIE <DANIE@192.168.10.11>
2024-11-19 20:59:54 -0500

    Tarea de 30 ejercicios de programacion

---
+++ src/tareaejercicios/Tareaejercicios.java
@@ -0,0 +1,239 @@
+
+package tareaejercicios;
+import java.util.Scanner;
+public class Tareaejercicios {
+
+    public static void main(String[] args) {
+        //System.out.println("Hola mundo..."); ejercicio 1 
+        
+        //System.out.println("254"); ejercicio 2
+        
+        //System.out.println("3.14"); ejercicio 3
+        
+        //int num1 = 1234; ejercicio 4
+        //int num2 = 532;
+        //int suma = num1 + num2;
+        //System.out.println("La suma es: " + suma);
+        
+        //int num1 = 1234; ejercicio 5
+        //int num2 = 532;
+        //int resta = num1 - num2;
+        //System.out.println("La resta es: " + resta);
+        
+        //int num1 = 1234; ejercicio 6
+        //int num2 = 532;
+        //int multiplicacion = num1 - num2;
+        //System.out.println("La multiplicacion es: " + multiplicacion);
+        
+        //int num1 = 1234; ejercicio 7
+        //int num2 = 532;
+        //int division = num1/num2;
+        //System.out.println("La division es: " + division);
+        
+        //for (int i = 1; i <= 3; i++) { ejercicio 8
+        // System.out.println(i);
+        
+        //for (int i = 1; i <= 9; i++) { ejercicio 9
+        //System.out.println(i);
+        
+        //for (int i = 1; i <= 10000; i++) { ejercicio 10
+        //System.out.println(i);
+        
+        //for (int i = 5; i <= 10; i++) { ejercicio 11
+        //System.out.println(i);
+        
+        //for (int i = 5; i <= 15; i++) { ejercicio 12
+        //System.out.println(i);
+        
+        //for (int i = 5; i <= 15000; i++) { ejercicio 13
+        //System.out.println(i);
+        
+        //for (int i = 1; i <= 200; i++) {ejercicio 14
+        //System.out.println("hola");
+        
+        //for (int i = 1; i <= 30; i++) { ejercicio 15
+        //System.out.println("Cuadrado de " + i + " es: " + (i * i));
+        
+        //long producto = 1; ejercicio 16 
+        //for (int i = 1; i <= 20; i++) {
+        //producto *= i;
+        //}
+        //System.out.println("El producto es: " + producto);
+        
+        //long suma = 0;  ejercicio 17
+        //for (int i = 1; i <= 100; i++) {
+        //suma += i * i;
+        //}
+        //System.out.println("La suma de los cuadrados es: " + suma);
+        
+        //Scanner scanner = new Scanner(System.in); ejercicio 18
+        //System.out.print("Introduce un número entero: ");
+        //int numero = scanner.nextInt();
+        //int suma = 0;
+        //for (int i = numero; i < numero + 100; i++) {
+        //suma += i;
+        //}
+        //System.out.println("La suma de los 100 siguientes números es: " + suma);
+        //scanner.close();
+        
+        
+        //Scanner scanner = new Scanner(System.in);    ejercicio 19
+        //System.out.print("Introduce la cantidad en euros: ");
+        //double euros = scanner.nextDouble();
+        //double tasaDeCambio = 1.06; //tasa de cambio actual
+        //double dolares = euros * tasaDeCambio;
+        //System.out.println(euros + " euros equivalen a " + dolares + " dolares.");
+        //scanner.close();
+   
+    
+        //Scanner scanner = new Scanner(System.in);     ejercicio 20
+        //System.out.print("Introduce la base del rectangulo: ");
+        //double base = scanner.nextDouble();
+        //System.out.print("Introduce la altura del rectangulo: ");
+        //double altura = scanner.nextDouble();
+        //double area = base * altura;
+        //System.out.println("El área del rectangulo es: " + area);
+        //scanner.close();
+    
+        
+        //Scanner scanner = new Scanner(System.in);    ejercicio 21
+        //System.out.print("Introduce el primer numero: ");
+        //int num1 = scanner.nextInt();
+        //System.out.print("Introduce el segundo numero: ");
+        //int num2 = scanner.nextInt();
+        //if (num1 > num2) {
+            //System.out.println("El mayor es: " + num1);
+            //System.out.println("El menor es: " + num2);
+        //} else if (num1 < num2) {
+           //System.out.println("El mayor es: " + num2);
+            //System.out.println("El menor es: " + num1);
+        //} else {
+            //System.out.println("Ambos números son iguales.");
+        //}
+        //scanner.close();
+        
+        //Scanner scanner = new Scanner(System.in);   ejercicio 22
+        //System.out.print("Introduce un numero entero: ");
+        //int numero = scanner.nextInt();
+        //System.out.println("Numeros impares menores que " + numero + ":");
+        //for (int i = 1; i < numero; i += 2) {
+            //System.out.println(i);
+        //}
+        //scanner.close();
+        
+        
+        //Scanner scanner = new Scanner(System.in);  ejercicio 23
+        //System.out.print("Introduce el primer numero: ");
+        //int num1 = scanner.nextInt();
+        //System.out.print("Introduce el segundo numero: ");
+        //int num2 = scanner.nextInt();
+        //while (num2 != 0) {
+            //int temp = num2;
+            //num2 = num1 % num2;
+            //num1 = temp;
+        //}
+        //System.out.println("El MCD es: " + num1);
+        //scanner.close();
+        
+        
+        //Scanner scanner = new Scanner(System.in);   ejercicio 24
+        //System.out.print("Introduce el coeficiente a: ");
+        //double a = scanner.nextDouble();
+        //System.out.print("Introduce el coeficiente b: ");
+        //double b = scanner.nextDouble();
+        //System.out.print("Introduce el coeficiente c: ");
+        //double c = scanner.nextDouble();
+        //double discriminante = b * b - 4 * a * c;
+        //if (discriminante > 0) {
+            //double x1 = (-b + Math.sqrt(discriminante)) / (2 * a);
+            //double x2 = (-b - Math.sqrt(discriminante)) / (2 * a);
+            //System.out.println("Las soluciones son: x1 = " + x1 + " y x2 = " + x2);
+        //} else if (discriminante == 0) {
+            //double x = -b / (2 * a);
+            //System.out.println("La solución unica es: x = " + x);
+        //} else {
+            //System.out.println("La ecuacion no tiene soluciones reales.");
+        //}
+        //scanner.close();
+        
+         
+        //int numero = 4;     ejercicio 25
+        //System.out.println("El factorial de " + numero + " es: " + factorial(numero));
+    //}
+    //public static int factorial(int n) {
+        //if (n == 0) {
+            //return 1;
+        //} else {
+            //return n * factorial(n - 1);
+        //}
+        
+        
+        //Scanner scanner = new Scanner(System.in);  ejercicio 26
+        //System.out.print("Introduzca el primer numero: ");
+        //int num1 = scanner.nextInt();
+        //System.out.print("Introduzca el segundo numero: ");
+        //int num2 = scanner.nextInt();
+        //System.out.print("Introduzca el tercer numero: ");
+        //int num3 = scanner.nextInt();
+        //int mayor = Math.max(num1, Math.max(num2, num3));
+        //int menor = Math.min(num1, Math.min(num2, num3));
+        //System.out.println("El mayor es: " + mayor);
+        //System.out.println("El menor es: " + menor);
+        //scanner.close();
+        
+        
+        //Scanner scanner = new Scanner(System.in);    ejercicio 27
+        //double temperatura;
+        //do {
+           //System.out.print("Introduce una temperatura en Fahrenheit (999 para salir): ");
+            //temperatura = scanner.nextDouble();
+            //if (temperatura != 999) {
+                //double celsius = (temperatura - 32) * 5 / 9;
+                //System.out.println("Equivale a: " + celsius + " 8C");
+            //}
+        //} while (temperatura != 999);
+        //scanner.close();
+        
+        
+         //for (int i = 1; i <= 3; i++) {   ejercicio 28 
+            //switch (i) {
+                //case 1:
+                    //System.out.println("Caso 1");
+                    //break;
+                //case 2:
+                    //System.out.println("Caso 2");
+                    //break;
+                //case 3:
+                    //System.out.println("Caso 3");
+                    //break;
+            //}
+        
+        
+         //Scanner scanner = new Scanner(System.in);
+        //System.out.println("Introduce datos (CTRL+D para terminar):");
+        //while (scanner.hasNext()) {
+            //System.out.println("Leido: " + scanner.nextLine());
+        //}
+        //scanner.close();
+        
+        
+        //for (int num = 2; num <= 50; num++) { 
+            //boolean esPrimo = true;
+
+            //for (int divisor = 2; divisor <= Math.sqrt(num); divisor++) {
+                //if (num % divisor == 0) {
+                    //esPrimo = false;
+                    //break;
+                //}
+            //}
+            //if (esPrimo) {
+                //System.out.println(num + " es primo.");
+            //}
+        //}
+        
+    //}
+    //}
+
+
+    
+
